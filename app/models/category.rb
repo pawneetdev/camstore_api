63b6c081f5c1b416adea22e00a4ff6c0e7730807
Pawneet <pawneet.dev@gmail.com>
2019-12-08 22:02:48 +0530

    Unit test for category

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,6 +3,7 @@ class Category < ApplicationRecord
 	validates :name, presence: true
 	validates :category_type, presence: true
 	validates :model, presence: true
+	validates_format_of :model, :with => /\d{4}/, :message => "^Must be of the format: yyyy"
 	has_many :products
 
 end
--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -26,4 +26,22 @@ class CategoryTest < ActiveSupport::TestCase
 		assert_not category.valid?
 	end
 
+	test "fails when model format is not yyyy" do
+		category = Category.new
+		category.name = "nikon"
+		category.category_type = "point and shoot"
+		category.model = 1
+
+		assert_not category.valid?
+	end
+
+	test "passes when model format is yyyy" do
+		category = Category.new
+		category.name = "nikon"
+		category.category_type = "point and shoot"
+		category.model = 2018
+
+		assert category.valid?
+	end
+
 end
